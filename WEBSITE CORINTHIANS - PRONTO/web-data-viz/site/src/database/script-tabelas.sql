c964ac389ed61861dfb7032c27e783b614132fbf
GyUliAmArTinsPiquEira <gyulia.piqueira@sptech.school>
2023-07-17 22:01:42 -0300

    finish

---
+++ WEBSITE CORINTHIANS - PRONTO/web-data-viz/site/src/database/script-tabelas.sql
@@ -0,0 +1,122 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+CREATE DATABASE Corinthians;
+USE Corinthians;
+
+CREATE TABLE Usuario(
+idUsuario INT primary key auto_increment, 
+nome VARCHAR(70),
+email VARCHAR(70), 
+estado VARCHAR(45), 
+genero VARCHAR(10),
+	constraint chkGenero check (genero in('feminino', 'masculino')),
+idade INT, 
+senha VARCHAR(20), 
+confirmacaoSenha VARCHAR(20)
+);
+
+CREATE TABLE Quiz (
+idQuiz int primary key auto_increment, 
+Nome VARCHAR(45), 
+TotalPerguntas INT
+);
+
+CREATE TABLE Resultado (
+idResultado INT auto_increment, 
+Acertos INT, 
+Erros INT, 
+fkUsuario INT, 
+	constraint fkUsuario foreign key (fkUsuario) references usuario(idUsuário), 
+fkQuiz int,
+    constraint fkQuiz foreign key (fkQuiz) references Quiz(idQuiz), 
+    constraint pkComposta primary key (idResultado, fkQuiz)
+);
+
+
+
+-- CREATE TABLE aviso (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	titulo VARCHAR(100),
+-- 	descricao VARCHAR(150),
+-- 	fk_usuario INT,
+-- 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+-- );
+
+-- create table aquario (
+-- /* em nossa regra de negócio, um aquario tem apenas um sensor */
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	descricao VARCHAR(300)
+-- );
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+-- create table medida (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	dht11_umidade DECIMAL,
+-- 	dht11_temperatura DECIMAL,
+-- 	luminosidade DECIMAL,
+-- 	lm35_temperatura DECIMAL,
+-- 	chave TINYINT,
+-- 	momento DATETIME,
+-- 	fk_aquario INT,
+-- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+-- );
+
+
+/*
+comando para sql server - banco remoto - ambiente de produção
+*/
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY IDENTITY(1,1),
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+);
+
+/*
+comandos para criar usuário em banco de dados azure, sqlserver,
+com permissão de insert + update + delete + select
+*/
+
+CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+DEFAULT_SCHEMA = dbo;
+
+EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
